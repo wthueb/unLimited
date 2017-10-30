c8f8d9e9f23b194ed941a64a42e20898d1ddf9d6
William Huebner <wilhueb@gmail.com>
2017-10-30 02:33:51 -0400

    added some options to glow

--- unLimited/gui.hpp
+++ unLimited/gui.hpp
@@ -11,7 +11,7 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
-#define VERSION_PATCH 20
+#define VERSION_PATCH 21
 
 namespace ImGui
 {
@@ -83,92 +83,144 @@ namespace gui
 
 			ImGui::PushItemWidth(150.f);
 
-			ImGui::BetterCheckbox("aimbot", &options::aim::aimbot);
-
-			// FIXMEW: null mouse input
-			static float aimbot_alpha = 0.f;
-			
-			if (!options::aim::aimbot)
-				aimbot_alpha = std::max(aimbot_alpha - .02f, .3f);
-			else
-				aimbot_alpha = std::min(1.f, aimbot_alpha + .02f);
-
-			ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, aimbot_alpha));
-
-			ImGui::SliderFloat("fov", &options::aim::fov, .1f, 180.f, "%.1f", 2.f);
-			ImGui::BetterCheckbox("smooth", &options::aim::smooth);
-			ImGui::SliderFloat("smooth amount", &options::aim::smooth_amount, 1.f, 30.f, "%.1f", 1.f);
-			
+			ImGui::BetterCheckbox("aim", &options::aim::enabled);
 			{
-				static int selected = 0;
-				if (ImGui::Combo("bone", &selected, [](void* data, int idx, const char** out_text)
+				static float aim_alpha = 0.f;
+
+				if (!options::aim::enabled)
+					aim_alpha = std::max(aim_alpha - .02f, .3f);
+				else
+					aim_alpha = std::min(1.f, aim_alpha + .02f);
+
+				ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, aim_alpha));
+
+				ImGui::BetterCheckbox("aimbot", &options::aim::aimbot);
 				{
-					*out_text = options::bones.at(idx).name;
-					return true;
-				}, nullptr, options::bones.size(), -1))
-					options::aim::bone = options::bones.at(selected).num;
+					// FIXMEW: null mouse input
+					static float aimbot_alpha = 0.f;
+
+					if (!options::aim::aimbot)
+						aimbot_alpha = std::max(aimbot_alpha - .02f, .3f);
+					else
+						aimbot_alpha = std::min(1.f, aimbot_alpha + .02f);
+
+					ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, aimbot_alpha));
+
+					ImGui::SliderFloat("fov", &options::aim::fov, .1f, 180.f, "%.1f", 2.f);
+					ImGui::BetterCheckbox("smooth", &options::aim::smooth);
+					ImGui::SliderFloat("smooth amount", &options::aim::smooth_amount, 1.f, 30.f, "%.1f", 1.f);
+
+					{
+						static int selected = 0;
+						if (ImGui::Combo("bone", &selected, [](void* data, int idx, const char** out_text)
+						{
+							*out_text = options::bones.at(idx).name;
+							return true;
+						}, nullptr, options::bones.size(), -1))
+							options::aim::bone = options::bones.at(selected).num;
+					}
+
+					ImGui::BetterCheckbox("on shoot", &options::aim::on_shoot);
+					ImGui::BetterCheckbox("reaim", &options::aim::reaim);
+
+					{
+						static int selected = 0;
+						if (ImGui::Combo("aim key", &selected, [](void* data, int idx, const char** out_text)
+						{
+							*out_text = options::keys.at(idx).name;
+							return true;
+						}, nullptr, options::keys.size(), -1))
+							options::aim::aim_key = options::keys.at(selected).num;
+					}
+
+					ImGui::BetterCheckbox("shoot teammates", &options::aim::friendlies);
+					ImGui::BetterCheckbox("visible check", &options::aim::visible_only);
+
+					ImGui::PopStyleVar();
+				}
+
+				ImGui::BetterCheckbox("rcs", &options::aim::rcs);
+
+				ImGui::PopStyleVar();
 			}
 
-			ImGui::BetterCheckbox("on shoot", &options::aim::on_shoot);
-			ImGui::BetterCheckbox("reaim", &options::aim::reaim);
-			
-			{
-				static int selected = 0;
-				if (ImGui::Combo("aim key", &selected, [](void* data, int idx, const char** out_text)
-				{
-					*out_text = options::keys.at(idx).name;
-					return true;
-				}, nullptr, options::keys.size(), -1))
-					options::aim::aim_key = options::keys.at(selected).num;
-			}
-
-			ImGui::BetterCheckbox("shoot teammates", &options::aim::friendlies);
-			ImGui::BetterCheckbox("visible check", &options::aim::visible_only);
-
-			ImGui::PopStyleVar();
-
-			ImGui::BetterCheckbox("rcs", &options::aim::rcs);
-			
 			ImGui::NextColumn();
+
+			ImGui::BetterCheckbox("esp", &options::esp::enabled);
+			{
+				static float esp_alpha = 0.f;
+
+				if (!options::esp::enabled)
+					esp_alpha = std::max(esp_alpha - .02f, .3f);
+				else
+					esp_alpha = std::min(1.f, esp_alpha + .02f);
+
+				ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, esp_alpha));
+
+				ImGui::BetterCheckbox("glow", &options::esp::glow);
+				{
+					static float glow_alpha = 0.f;
+
+					if (!options::esp::glow)
+						glow_alpha = std::max(glow_alpha - .02f, .3f);
+					else
+						glow_alpha = std::min(1.f, glow_alpha + .02f);
+
+					ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, glow_alpha));
+
+					if (ImGui::InputFloat("glow alpha", &options::esp::glow_alpha, .1f, 0.f, 1))
+						options::esp::glow_alpha = std::clamp(options::esp::glow_alpha, .1f, 1.f);
+
+					if (ImGui::InputInt("glow style", &options::esp::glow_style))
+						options::esp::glow_style = std::clamp(options::esp::glow_style, 0, 3);
+
+					ImGui::PopStyleVar();
+				}
+
+				ImGui::PopStyleVar();
+			}
+
 			ImGui::NextColumn();
 
 			ImGui::BetterCheckbox("backtracking", &options::misc::backtracking);
-
-			static float backtracking_alpha = 0.f;
-
-			if (!options::misc::backtracking)
-				backtracking_alpha = std::max(backtracking_alpha - .02f, .3f);
-			else
-				backtracking_alpha = std::min(1.f, backtracking_alpha + .02f);
-
-			ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, backtracking_alpha));
-
-			ImGui::BetterCheckbox("backtracking visual", &options::misc::backtracking_vis);
-
-			ImGui::PopStyleVar();
-
-			ImGui::BetterCheckbox("airstuck", &options::misc::airstuck);
-
-			static float airstuck_alpha = 0.f;
-
-			if (!options::misc::airstuck)
-				airstuck_alpha = std::max(airstuck_alpha - .02f, .3f);
-			else
-				airstuck_alpha = std::min(1.f, airstuck_alpha + .02f);
-
-			ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, airstuck_alpha));
-			
 			{
-				static int selected = 0;
-				if (ImGui::Combo("airstuck key", &selected, [](void* data, int idx, const char** out_text)
-				{
-					*out_text = options::keys.at(idx).name;
-					return true;
-				}, nullptr, options::keys.size(), -1))
-					options::misc::airstuck_key = options::keys.at(selected).num;
+				static float backtracking_alpha = 0.f;
+
+				if (!options::misc::backtracking)
+					backtracking_alpha = std::max(backtracking_alpha - .02f, .3f);
+				else
+					backtracking_alpha = std::min(1.f, backtracking_alpha + .02f);
+
+				ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, backtracking_alpha));
+
+				ImGui::BetterCheckbox("backtracking visual", &options::misc::backtracking_vis);
+
+				ImGui::PopStyleVar();
 			}
 
-			ImGui::PopStyleVar();
+			ImGui::BetterCheckbox("airstuck", &options::misc::airstuck);
+			{
+				static float airstuck_alpha = 0.f;
+
+				if (!options::misc::airstuck)
+					airstuck_alpha = std::max(airstuck_alpha - .02f, .3f);
+				else
+					airstuck_alpha = std::min(1.f, airstuck_alpha + .02f);
+
+				ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, airstuck_alpha));
+
+				{
+					static int selected = 0;
+					if (ImGui::Combo("airstuck key", &selected, [](void* data, int idx, const char** out_text)
+					{
+						*out_text = options::keys.at(idx).name;
+						return true;
+					}, nullptr, options::keys.size(), -1))
+						options::misc::airstuck_key = options::keys.at(selected).num;
+				}
+
+				ImGui::PopStyleVar();
+			}
 
 			if (ImGui::InputFloat("view fov", &options::misc::fov, 1.f, 0.f, 0))
 				options::misc::fov = std::clamp(options::misc::fov, 5.f, 179.f);
@@ -183,25 +235,26 @@ namespace gui
 
 			static bool dark_mode = false;
 			ImGui::BetterCheckbox("dark mode", &dark_mode);
-
-			static bool old_dark_mode = false;
-
-			if (dark_mode != old_dark_mode)
 			{
-				for (auto i = 0; i < ImGuiCol_COUNT; ++i)
+				static bool old_dark_mode = false;
+
+				if (dark_mode != old_dark_mode)
 				{
-					ImVec4 &col = style.Colors[i];
+					for (auto i = 0; i < ImGuiCol_COUNT; ++i)
+					{
+						ImVec4 &col = style.Colors[i];
 
-					float hue, saturation, value;
-					ImGui::ColorConvertRGBtoHSV(col.x, col.y, col.z, hue, saturation, value);
+						float hue, saturation, value;
+						ImGui::ColorConvertRGBtoHSV(col.x, col.y, col.z, hue, saturation, value);
 
-					if (saturation < .1f)
-						value = 1.f - value;
+						if (saturation < .1f)
+							value = 1.f - value;
 
-					ImGui::ColorConvertHSVtoRGB(hue, saturation, value, col.x, col.y, col.z);
+						ImGui::ColorConvertHSVtoRGB(hue, saturation, value, col.x, col.y, col.z);
+					}
+
+					old_dark_mode = dark_mode;
 				}
-
-				old_dark_mode = dark_mode;
 			}
 
 			ImGui::SameLine(ImGui::GetWindowWidth() / 2 - ImGui::CalcTextSize(version).x / 2);
