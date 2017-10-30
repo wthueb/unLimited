3cc2e646f4a1a47d1836d047fb120b783cef5ef7
William Huebner <wilhueb@gmail.com>
2017-10-29 22:47:16 -0400

    actually fixed the stuttering

--- unLimited/hooks/create_move.cpp
+++ unLimited/hooks/create_move.cpp
@@ -8,12 +8,8 @@
 
 bool __stdcall hooks::hk_create_move(float sample_input_frametime, CUserCmd* cmd)
 {
-	static auto o_create_move = client_mode_hook->get_original<bool(__thiscall*)(void*, float, CUserCmd*)>(index::create_move);
-
-	auto ret = o_create_move(g_client_mode, sample_input_frametime, cmd);
-
 	if (!cmd || !cmd->command_number)
-		return false;
+		return true;
 
 	uintptr_t* frame_ptr;
 	__asm mov frame_ptr, ebp
