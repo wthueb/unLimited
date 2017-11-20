0a38e9b8fe8190f7119c31f34cb2a9ae33217f0b
William Huebner <wilhueb@gmail.com>
2017-11-20 01:31:36 -0500

    disable input when section faded in gui

--- unLimited/gui.hpp
+++ unLimited/gui.hpp
@@ -10,28 +10,12 @@
 #include "options.hpp"
 
 #ifndef GUI_TEST
-	#include "utils.hpp"
+#include "utils.hpp"
 #endif
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 1
-#define VERSION_PATCH 0
-
-#define PFADE(var, time) {                                                                        \
-	static float alpha = .3f;                                                                     \
-	static bool old = var;                                                                        \
-	static float start_alpha = alpha;                                                             \
-	static float start_time = ImGui::GetTime();                                                   \
-	if (var != old) {                                                                             \
-		start_alpha = alpha;                                                                      \
-		start_time = ImGui::GetTime();                                                            \
-		old = var;                                                                                \
-	}                                                                                             \
-	if (!var)                                                                                     \
-		alpha = std::clamp(start_alpha - .7f * (ImGui::GetTime() - start_time) / time, .3f, 1.f); \
-	else                                                                                          \
-		alpha = std::clamp(start_alpha + .7f * (ImGui::GetTime() - start_time) / time, .3f, 1.f); \
-	ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, alpha)); }
+#define VERSION_PATCH 1
 
 namespace ImGui
 {
@@ -80,6 +64,39 @@ namespace ImGui
 
 		return pressed;
 	}
+
+	void BetterPopItemFlag()
+	{
+		ImGuiWindow* window = GetCurrentWindow();
+		if (window->DC.ItemFlagsStack.size())
+			window->DC.ItemFlagsStack.pop_back();
+		window->DC.ItemFlags = window->DC.ItemFlagsStack.empty() ? ImGuiItemFlags_Default_ : window->DC.ItemFlagsStack.back();
+	}
+}
+
+#define DISABLE(var, fade_time) {                                                                           \
+	static float alpha = .3f;                                                                               \
+	static bool old = var;                                                                                  \
+	static float start_alpha = alpha;                                                                       \
+	static float start_time = ImGui::GetTime();                                                             \
+                                                                                                            \
+	if (var != old)                                                                                         \
+	{                                                                                                       \
+		start_alpha = alpha;                                                                                \
+		start_time = ImGui::GetTime();                                                                      \
+		old = var;                                                                                          \
+	}                                                                                                       \
+                                                                                                            \
+	if (!var)                                                                                               \
+		alpha = std::clamp(start_alpha - .7f * (ImGui::GetTime() - start_time) / fade_time, .3f, 1.f);      \
+	else                                                                                                    \
+		alpha = std::clamp(start_alpha + .7f * (ImGui::GetTime() - start_time) / fade_time, .3f, 1.f);      \
+                                                                                                            \
+	ImGui::PushStyleVar(ImGuiStyleVar_Alpha, std::min(style.Alpha, alpha));                                 \
+                                                                                                            \
+	ImGuiWindow* window = ImGui::GetCurrentWindow();                                                        \
+	if (!window->DC.ItemFlagsStack.size() || !(window->DC.ItemFlagsStack.back() & ImGuiItemFlags_Disabled)) \
+		ImGui::PushItemFlag(ImGuiItemFlags_Disabled, !var);                                                 \
 }
 
 namespace gui
@@ -87,9 +104,9 @@ namespace gui
 	static char title[64];
 	static bool dark_mode = false;
 
-	static void setup_style(const ImVec4 &theme_color)
+	static void setup_style(const ImVec4& theme_color)
 	{
-		static auto &style = ImGui::GetStyle();
+		static auto& style = ImGui::GetStyle();
 
 		style.Colors[ImGuiCol_Text] = ImVec4{ .05f, .05f, .05f, 1.f };
 		style.Colors[ImGuiCol_TextDisabled] = style.Colors[ImGuiCol_Text];
@@ -125,7 +142,7 @@ namespace gui
 		{
 			for (auto i = 0; i < ImGuiCol_COUNT; ++i)
 			{
-				ImVec4 &col = style.Colors[i];
+				ImVec4& col = style.Colors[i];
 
 				float hue, saturation, value;
 				ImGui::ColorConvertRGBtoHSV(col.x, col.y, col.z, hue, saturation, value);
@@ -157,17 +174,24 @@ namespace gui
 
 			ImGui::BetterCheckbox("aim", &options::aim::enabled);
 			{
-				// FIXMEW: null mouse input
-				PFADE(options::aim::enabled, .2f);
+				DISABLE(options::aim::enabled, .2f);
 
 				ImGui::BetterCheckbox("aimbot", &options::aim::aimbot);
 				{
-					PFADE(options::aim::aimbot, .2f);
+					DISABLE(options::aim::aimbot, .2f);
 
 					ImGui::SliderFloat("fov", &options::aim::fov, .1f, 180.f, "%.1f", 2.f);
 					ImGui::BetterCheckbox("smooth", &options::aim::smooth);
-					ImGui::SliderFloat("smooth amount", &options::aim::smooth_amount, 1.f, 30.f, "%.1f", 1.f);
+					{
+						DISABLE(options::aim::smooth, .2f);
 
+						ImGui::SliderFloat("smooth amount", &options::aim::smooth_amount, 1.f, 30.f, "%.1f", 1.f);
+
+						ImGui::PopStyleVar();
+						ImGui::BetterPopItemFlag();
+					}
+
+					// bone
 					{
 						static int selected = 0;
 						if (ImGui::Combo("bone", &selected, [](void* data, int idx, const char** out_text)
@@ -181,6 +205,7 @@ namespace gui
 					ImGui::BetterCheckbox("on shoot", &options::aim::on_shoot);
 					ImGui::BetterCheckbox("reaim", &options::aim::reaim);
 
+					// aim key
 					{
 						static int selected = 0;
 						if (ImGui::Combo("aim key", &selected, [](void* data, int idx, const char** out_text)
@@ -195,22 +220,24 @@ namespace gui
 					ImGui::BetterCheckbox("visible check", &options::aim::visible_only);
 
 					ImGui::PopStyleVar();
+					ImGui::BetterPopItemFlag();
 				}
 
 				ImGui::BetterCheckbox("rcs", &options::aim::rcs);
 
 				ImGui::PopStyleVar();
+				ImGui::BetterPopItemFlag();
 			}
 
 			ImGui::NextColumn();
 
 			ImGui::BetterCheckbox("esp", &options::esp::enabled);
 			{
-				PFADE(options::esp::enabled, .2f);
+				DISABLE(options::esp::enabled, .2f);
 
 				ImGui::BetterCheckbox("glow", &options::esp::glow);
 				{
-					PFADE(options::esp::glow, .2f);
+					DISABLE(options::esp::glow, .2f);
 
 					if (ImGui::InputFloat("glow alpha", &options::esp::glow_alpha, .1f, 0.f, 1))
 						options::esp::glow_alpha = std::clamp(options::esp::glow_alpha, .1f, 1.f);
@@ -219,21 +246,24 @@ namespace gui
 						options::esp::glow_style = std::clamp(options::esp::glow_style, 0, 3);
 
 					ImGui::PopStyleVar();
+					ImGui::BetterPopItemFlag();
 				}
 
 				ImGui::BetterCheckbox("radar", &options::esp::radar);
 
 				ImGui::PopStyleVar();
+				ImGui::BetterPopItemFlag();
 			}
 
 			ImGui::NextColumn();
 
 			ImGui::BetterCheckbox("backtracking", &options::misc::backtracking);
 			{
-				PFADE(options::misc::backtracking, .2f);
+				DISABLE(options::misc::backtracking, .2f);
 
 				ImGui::BetterCheckbox("backtracking visual", &options::misc::backtracking_vis);
 
+				// backtracking amount
 				{
 					ImGui::PushItemWidth(30.f);
 
@@ -241,19 +271,20 @@ namespace gui
 
 					// so we don't have the + -, kind of stupid but
 					if (ImGui::InputFloat("backtracking amount (ticks, max 12)", &temp, 0.f, 0.f, 0))
-					{
 						options::misc::backtracking_amt = std::clamp(int(temp), 1, 12);
-					}
-					
+
 					ImGui::PopItemWidth();
 				}
+
 				ImGui::PopStyleVar();
+				ImGui::BetterPopItemFlag();
 			}
 
 			ImGui::BetterCheckbox("airstuck", &options::misc::airstuck);
 			{
-				PFADE(options::misc::airstuck, .2f);
+				DISABLE(options::misc::airstuck, .2f);
 
+				// airstuck key
 				{
 					static int selected = 0;
 					if (ImGui::Combo("airstuck key", &selected, [](void* data, int idx, const char** out_text)
@@ -265,6 +296,7 @@ namespace gui
 				}
 
 				ImGui::PopStyleVar();
+				ImGui::BetterPopItemFlag();
 			}
 
 			if (ImGui::InputFloat("view fov", &options::misc::fov, 1.f, 0.f, 0))
@@ -275,9 +307,9 @@ namespace gui
 			ImGui::BetterCheckbox("show ranks", &options::misc::show_ranks);
 
 			ImGui::PopItemWidth();
-			
+
 			ImGui::Columns(1);
-			
+
 			ImGui::SetCursorPosY(ImGui::GetWindowHeight() - ImGui::CalcTextSize("").y - 10);
 
 			ImGui::BetterCheckbox("dark mode", &dark_mode);
@@ -288,7 +320,7 @@ namespace gui
 				{
 					for (auto i = 0; i < ImGuiCol_COUNT; ++i)
 					{
-						ImVec4 &col = style.Colors[i];
+						ImVec4& col = style.Colors[i];
 
 						float hue, saturation, value;
 						ImGui::ColorConvertRGBtoHSV(col.x, col.y, col.z, hue, saturation, value);
@@ -304,11 +336,12 @@ namespace gui
 			}
 
 			ImGui::SameLine();
-
-			static float theme_color[3] = { .65f, .85f, .95f };
-			if (ImGui::ColorEdit3("gui theme color", theme_color, ImGuiColorEditFlags_NoOptions | ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip))
-				setup_style(ImVec4{ theme_color[0], theme_color[1], theme_color[2], 1.f });
 			
+			static float theme_color[3] = { .65f, .85f, .95f };
+			if (ImGui::ColorEdit3("theme color", theme_color, ImGuiColorEditFlags_NoOptions | ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoTooltip))
+				setup_style(ImVec4{ theme_color[0], theme_color[1], theme_color[2], 1.f });
+
+			// unload button
 			{
 				auto label_size = ImGui::CalcTextSize("unload");
 
@@ -335,7 +368,7 @@ namespace gui
 			ImGui::SameLine(ImGui::GetWindowWidth() - ImGui::CalcTextSize("http://wi1.xyz/").x - 10.f);
 
 			ImGui::Text("http://wi1.xyz/");
-			
+
 			ImGui::End();
 		}
 	}
@@ -345,7 +378,7 @@ namespace gui
 		snprintf(title, sizeof(title), "universeL by wi1 | v%i.%i.%i", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
 
 		auto& io = ImGui::GetIO();
-		
+
 		io.Fonts->AddFontDefault();
 		auto font = io.Fonts->AddFontFromMemoryCompressedTTF(RobotoFont_compressed_data, RobotoFont_compressed_size, 14.f);
 		io.FontDefault = font;
