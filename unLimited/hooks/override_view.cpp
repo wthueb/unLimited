bab68eeaeae63ec2f91252d3528d0861a33eb146
William Huebner <wilhueb@gmail.com>
2017-12-14 01:27:39 -0500

    fixed crashing with fov

--- unLimited/hooks/override_view.cpp
+++ unLimited/hooks/override_view.cpp
@@ -16,9 +16,9 @@ void __stdcall hooks::hk_override_view(CViewSetup* view)
 	if (!localplayer)
 		return o_override_view(g_client_mode, view);
 
-	if (localplayer->GetObserverTarget().IsValid() ? !localplayer->GetObserverTarget()->IsScoped() : !localplayer->IsScoped())
+	if (localplayer->IsAlive() && (!localplayer->IsScoped() || options.misc_fov_scoped))
 		view->fov = options.misc_fov;
-	else if (options.misc_fov_scoped)
+	else if (localplayer->GetObserverTarget().IsValid() && (!localplayer->GetObserverTarget()->IsScoped() || options.misc_fov_scoped))
 		view->fov = options.misc_fov;
 
 	visuals::thirdperson_override_view();
