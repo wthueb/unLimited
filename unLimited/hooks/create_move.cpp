fe37fbf90f4bd9112c127bc2854cce9213657e85
William Huebner <wilhueb@gmail.com>
2018-02-20 02:57:11 -0500

    i'm told this is more correct

--- unLimited/hooks/create_move.cpp
+++ unLimited/hooks/create_move.cpp
@@ -1,20 +1,21 @@
 #include "hooks.hpp"
 
+#include <intrin.h>
+
 #include "../features/features.hpp"
 #include "../math.hpp"
 #include "../vmt_hook.hpp"
 
 bool __stdcall hooks::hk_create_move(float sample_input_frametime, CUserCmd* cmd)
 {
-	static auto o_create_move = client_mode_hook->get_original<bool(__thiscall*)(void*, float, CUserCmd*)>(index::create_move);
+	static const auto o_create_move = client_mode_hook->get_original<bool(__thiscall*)(void*, float, CUserCmd*)>(index::create_move);
 
 	if (!cmd || !cmd->command_number)
 		return o_create_move(g_client_mode, sample_input_frametime, cmd);
 	
-	uintptr_t* frame_ptr;
-	__asm mov frame_ptr, ebp
+	auto ebp = reinterpret_cast<uintptr_t*>(uintptr_t(_AddressOfReturnAddress()) - sizeof(void*));
 
-	bool& send_packet = *reinterpret_cast<bool*>(*frame_ptr - 0x1C);
+	bool& send_packet = *reinterpret_cast<bool*>(*ebp - 0x1C);
 
 	backtracking::process(cmd);
 	aimbot::process(cmd, send_packet);
@@ -23,9 +24,9 @@ bool __stdcall hooks::hk_create_move(float sample_input_frametime, CUserCmd* cmd
 	misc::airstuck(cmd);
 	misc::show_ranks(cmd);
 
-	QAngle old_angle = cmd->viewangles;
-	float old_forward = cmd->forwardmove;
-	float old_side = cmd->sidemove;
+	const QAngle old_angle = cmd->viewangles;
+	const float old_forward = cmd->forwardmove;
+	const float old_side = cmd->sidemove;
 
 	antiaim::process(cmd, send_packet);
 
